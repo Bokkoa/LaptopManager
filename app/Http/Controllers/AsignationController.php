c7d67a442fabb994e77b091f3b5228bd96f61ad0
Kone Cruz <krivozznova@gmail.com>
2020-04-19 00:54:19 -0500

    First commit

---
+++ app/Http/Controllers/AsignationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Asignation;
+use Illuminate\Http\Request;
+
+class AsignationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Asignation  $asignation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Asignation $asignation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Asignation  $asignation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Asignation $asignation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Asignation  $asignation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Asignation $asignation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Asignation  $asignation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Asignation $asignation)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/EntranceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Entrance;
+use Illuminate\Http\Request;
+
+class EntranceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Entrance  $entrance
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Entrance $entrance)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Entrance  $entrance
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Entrance $entrance)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Entrance  $entrance
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Entrance $entrance)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Entrance  $entrance
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Entrance $entrance)
+    {
+        //
+    }
+}
